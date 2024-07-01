09bd11beef3e530aebee0b06b7956165733d60ab
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-07-01 20:51:55 -0300

    Funcão com parâmetro

---
+++ funções/funcao-paramentro.js
@@ -0,0 +1,11 @@
+/*
+     Função: Trecho de código que só é executado quando é chamado/invocado
+
+        Função Void (Vazia)
+        Função com parâmetros (s)
+        Função  return
+        Função Arrow / Arrow Function
+
+*/
+
+
