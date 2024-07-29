3a2423acb401658056d0585f2aef107f5fb0d3a2
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-07-29 18:42:49 -0300

    Update exe01.js

--- indexOf em Array/exe01/exe01.js
+++ indexOf em Array/exe01/exe01.js
@@ -1,1 +1,7 @@
-console.log('Olá,Mundo!')
+function encontrarIndice(numeros, numeroProcurado){
+    return  numeros.indexOf(numeroProcurado)
+    
+}
+
+
+console.log(encontrarIndice([12,34,56,78,90,56],90))
