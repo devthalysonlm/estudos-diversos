d170cffc7d5685966f2a65da54ded9ea95e4a248
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-08-02 18:45:12 -0300

    exe01
    exe01 ok!

---
+++ funções/exe01.js
@@ -0,0 +1,20 @@
+//exercício-01
+
+function subtrair(a,b){
+    let resultado = a - b
+    return  resultado
+}
+
+let diferenca = subtrair(10,7)
+console.log(diferenca)
+
+//Qual a diferença de uma para outra?
+
+function subtrair(a,b){
+    let diferenca  = a - b
+    return diferenca
+}
+
+console.log(subtrair(10,7))
+
+
