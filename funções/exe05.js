e9c7ff19b54aaa92b0a54ddd0155b2f02d012571
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-08-02 21:54:32 -0300

    exe05
    exe05 ok!

---
+++ funções/exe05.js
@@ -0,0 +1,8 @@
+// exercício-05
+
+ function perimetroRetangulo(altura=0, largura=0){
+    let perimetro =  2 * (altura + largura)
+    return perimetro
+ }
+
+ console.log(perimetroRetangulo(5,10))
