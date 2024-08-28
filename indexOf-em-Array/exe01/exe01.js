12e1409406070b4ad7bbd3f4fb08a7a5253d7293
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-08-28 17:14:57 -0300

    Retomando

--- indexOf-em-Array/exe01/exe01.js
+++
@@ -1,7 +0,0 @@
-function encontrarIndice(numeros, numeroProcurado){
-    return  numeros.indexOf(numeroProcurado)
-    
-}
-
-
-console.log(encontrarIndice([12,34,56,78,90,56],90))
--- indexOf-em-Array/exe02/exe02.js
+++
@@ -1,9 +0,0 @@
-
-
-function contarOcorrencias(numeros, numeroProcurado){
-    
-    return 
-}
-
-
-console.log(contarOcorrencias([10,20,30,40],40))
