590b3e397b3bfa9f117d9259e8669f4e135692c1
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-08-28 17:25:14 -0300

    Abordagem de estudo sobre array

---
+++ funcoes-de-array/abordagem-de-estudo/index.js
@@ -0,0 +1,29 @@
+/*Podemos criar um array usando colchetes [];
+
+
+let numeros = [1,2,3,4,5]
+let frutas = ['maça','Morando','abacate']
+
+
+
+
+//Podemos criar utulizando construtor Array();
+
+let numeros = new Array(1,2,3,4,5)
+
+console.log(numeros)
+
+Primeiros vamos falar de cada  valor e depois trataremos em exercícios cada um deles.
+
+
+
+let frutas = new Array('Maça','Banana','Pera')
+//Array normal 
+console.log(frutas)
+
+frutas.push('Manga')
+//Array atualizado
+
+console.log(frutas)
+*/
+
