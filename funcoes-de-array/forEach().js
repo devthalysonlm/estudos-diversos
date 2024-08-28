eb0d63365e96b792fab6a53026fdf32f54538c8d
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-08-28 17:55:21 -0300

    part02

---
+++ funcoes-de-array/forEach().js
@@ -0,0 +1,7 @@
+//executa uma função para cada elemento de um array:
+
+let frutas = new Array('Laranja','Abacaxi','Maça','Pera')
+
+frutas.forEach(function(fruta){
+    console.log(fruta)
+})
---
+++ funcoes-de-array/length().js
@@ -0,0 +1,5 @@
+//Retorna o número de elementos de um array
+
+let numeros = new Array(1,2,3,4,5,6,7,8)
+
+console.log(numeros.length)
