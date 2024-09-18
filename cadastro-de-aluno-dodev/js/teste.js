a5e50350f44afb0ac3d1f5632173c8a28102c965
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-09-18 19:38:58 -0300

    Create teste.js

---
+++ cadastro-de-aluno-dodev/js/teste.js
@@ -0,0 +1,29 @@
+
+
+/*
+
+class Aluno {
+    Nome
+    Idade
+    Nota
+    constructor(nome,idade,nota){
+        this.Nome = nome
+        this.Idade = idade
+        this.Nota = nota
+    }
+}
+
+let arrayAlunos = []
+
+function CadastrarAluno(nome,idade,array){
+    let ObjetoAluno = new Aluno(nome,idade,nota)
+    if(!array.some(x => x.Nome == nome)){
+        array.push(ObjetoAluno)
+    }
+    return ObjetoAluno
+}
+
+CadastrarAluno('Thalyson',25,10,arrayAlunos)
+console.log(CadastrarAluno)
+
+*/
