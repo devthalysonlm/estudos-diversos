a4c0676a7169517dfb0b4af6f2ef0aa85d826acc
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-10-14 20:35:43 -0300

    Update index.js

--- testes-diversos/index.js
+++ testes-diversos/index.js
@@ -1,5 +1,20 @@
-if(!arrayAlunos.some(x => x.Nome === nome)){
-    Array.push(objetoAluno)
-}
-
-return objetoAluno
+class Aluno{
+    nome
+    idade
+    nota
+    constructor(nome,idade,nota){
+      this.Nome = nome
+      this.Idade = idade
+      this.Nota = nota
+      
+    }
+  }
+   let arrayAlunos = [];
+  
+   function CadastrarAluno(nome, idade, nota, array){
+    let obejtoAluno = new Aluno(nome, idade, nota)
+    if(!array.some(x => x.Nome === nome)){
+      array.push(obejtoAluno)
+    }
+    return obejtoAluno;
+   }
