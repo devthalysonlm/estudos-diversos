5332ee34e18bdf304cc47891ddfd90e9f71644ad
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-10-15 21:33:05 -0300

    Praticando
    Praticando

--- testes-diversos/index.js
+++ testes-diversos/index.js
@@ -1,20 +1,26 @@
-class Aluno{
-    nome
-    idade
-    nota
-    constructor(nome,idade,nota){
-      this.Nome = nome
-      this.Idade = idade
-      this.Nota = nota
-      
-    }
-  }
-   let arrayAlunos = [];
-  
-   function CadastrarAluno(nome, idade, nota, array){
-    let obejtoAluno = new Aluno(nome, idade, nota)
-    if(!array.some(x => x.Nome === nome)){
-      array.push(obejtoAluno)
-    }
-    return obejtoAluno;
-   }
+
+
+let arrayColaboradores = [
+  {Nome:'Abalak chalá', Idade: 44, Nota: 8.9},
+  {Nome:'Abur habila', Idade: 41, Nota: 8},
+  {Nome:'HalBilau', Idade: 43, Nota: 8.7},
+  {Nome:'AmonikRabu', Idade: 42, Nota: 8.2},
+  {Nome:'MohamedSala', Idade: 40, Nota: 8.1},
+  {Nome:'HashamSalk', Idade: 45, Nota: 8.6}
+];
+
+
+function OrdenarPorNota(array){
+  array.sort((a, b) => a.Nota - b.Nota)
+  return array
+}
+
+function OrdenarPorIdade(array){
+  array.sort((a, b) => b.Idade - a.Idade)
+  return array
+}
+
+
+
+console.log(OrdenarPorNota(arrayColaboradores))
+console.log(OrdenarPorIdade(arrayColaboradores))
