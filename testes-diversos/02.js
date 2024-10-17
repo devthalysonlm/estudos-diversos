bd11bf85b6d506b72b4dd0831aec3afc2a74053b
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-10-17 20:50:31 -0300

    Revisão

--- testes-diversos/02.js
+++ testes-diversos/02.js
@@ -1,5 +1,14 @@
 
-let nomes = ['Thalyson','Lorena','Douglas','Zilda','Aginor','Peidinho','Nina','Safira','Tobias']
+let nomes = [
+    {Nome:'Nina', Idade: 4, Nota: 10},
+    {Nome:'Safyra', Idade: 3, Nota: 9},
+    {Nome:'Luna', Idade: 2, Nota: 8},
+    {Nome:'Tobias', Idade: 1, Nota: 7}
+]
+
+nomes.forEach((morfetos) => {
+    console.log(morfetos.Nome)
+})
 
 
 
