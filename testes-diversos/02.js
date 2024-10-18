acfcf308693e31bb6f4f6eef5358aa75cfccc659
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-10-17 21:06:27 -0300

    Update 02.js

--- testes-diversos/02.js
+++ testes-diversos/02.js
@@ -1,14 +1,40 @@
 
+//Criando um array de objetos
 let nomes = [
     {Nome:'Nina', Idade: 4, Nota: 10},
     {Nome:'Safyra', Idade: 3, Nota: 9},
-    {Nome:'Luna', Idade: 2, Nota: 8},
-    {Nome:'Tobias', Idade: 1, Nota: 7}
+    {Nome:'Peidinho', Idade: 2, Nota: 8},
+    {Nome:'Hael Preto', Idade: 1, Nota: 7}
 ]
 
-nomes.forEach((morfetos) => {
-    console.log(morfetos.Nome)
-})
+//Ordenando por nome
+
+function OrdenarPorNome(array){
+    array.sort((a, b) => {
+        const nomeA = a.Nome.toUpperCase()
+        const nomeB = b.Nome.toUpperCase()
+
+        if(nomeA < nomeB){
+            return -1
+        }
+        if(nomeA > nomeB){
+            return 1
+        }
+        return 0
+    })
+    return array
+}
 
 
 
+function OrdenarPorIdade(array){
+    array.sort((a, b) => a.Idade - b.Idade)
+    return array
+}
+
+
+
+
+
+console.log(OrdenarPorNome(nomes))
+console.log(OrdenarPorIdade(nomes))
